b65a6acb31f4eda7bf3622d57463847827a19cd8
Michael Lee <michaellee1019@gmail.com>
2024-02-05 17:50:16 -0500

    use jpg

--- src/models.py
+++ src/models.py
@@ -456,7 +456,7 @@ class PrusaConnectCameraSnapshot(Generic):
                     config = self.cameras_config.get(camera.name)
 
                     image_bytes = io.BytesIO()
-                    image.save(image_bytes, format='PNG')
+                    image.save(image_bytes, format='JPG')
 
                     resp = requests.put(
                         "https://connect.prusa3d.com/c/snapshot",
